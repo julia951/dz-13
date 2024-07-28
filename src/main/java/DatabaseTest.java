93398d350510ecae39522ec1df943407d888bc70
Julia <klimuk.julia@gmail.com>
2024-07-28 11:40:17 +0300

    add code

---
+++ src/main/java/DatabaseTest.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseTest {
+    private static final String URL = "jdbc:mysql://localhost:3306/your-database";
+    private static final String USER = "your-username";
+    private static final String PASSWORD = "your-password";
+}
